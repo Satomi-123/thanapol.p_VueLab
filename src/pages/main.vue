b1322829d8dfa6b6258515479c55958263c3057d
Satomi-123 <71164815+Satomi-123@users.noreply.github.com>
2021-05-27 16:58:24 +0700

    Initial commit

---
+++ src/pages/main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <manufacturer v-slot="data">
+      <devices v-bind:manufacturerId="data.factId"></devices>
+    </manufacturer>
+    <!-- <Manufacturer /> -->
+  </div>
+</template>
+
+<script>
+import Manufacturer from "@/components/Manufacturer.vue"; //@ = SRC
+import Devices from "@/components/Devices.vue";
+export default {
+  //name: "App", เพ่ือให้ดึงชื่อ main มาใช้
+  components: {
+    Manufacturer: Manufacturer,
+    Devices: Devices,
+  },
+};
+</script>
